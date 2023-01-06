bbb280b8f1b99e2687ce1069edfd7ba6a01dfc3f
Srujan <srujanmhase5@gmail.com>
2023-01-06 23:48:07 +0530

    fontstyles for app

---
+++ lib/constants/fontstyles.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle homeHeader = GoogleFonts.montserrat(
+  fontSize: 36,
+  color: Colors.white,
+);
+
+TextStyle currencyStyle = GoogleFonts.montserrat(
+  fontSize: 16,
+  color: const Color(0Xff5E5E5E),
+);
+
+TextStyle ctaStyle = GoogleFonts.montserrat(
+  fontSize: 20,
+  color: Colors.white,
+);
+
+TextStyle subtitleStyle = GoogleFonts.montserrat(
+  fontSize: 20,
+);
