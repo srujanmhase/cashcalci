2acc9e3be8daf68e404903b4f044f851cf5ac9e4
Srujan <srujanmhase5@gmail.com>
2023-01-06 23:12:57 +0530

    adding blocprovider in app

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,22 +1,26 @@
+import 'package:cashcalci/app/cubit/app_cubit.dart';
 import 'package:cashcalci/app/view/home.dart';
 import 'package:cashcalci/l10n/l10n.dart';
+import 'package:cashcalci/providers/rate_provider.dart';
+import 'package:cashcalci/providers/symbols_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
-        colorScheme: ColorScheme.fromSwatch(
-          accentColor: const Color(0xFF13B9FF),
-        ),
+    return BlocProvider(
+      create: (context) => AppCubit(
+        context.read<RateProvider>(),
+        context.read<SymbolsProvider>(),
       ),
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
-      home: const HomePage(),
+      child: const MaterialApp(
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+        home: HomePage(),
+      ),
     );
   }
 }
