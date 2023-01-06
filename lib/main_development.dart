29db859ea1d641eb998d7338c53474a45d3b06bc
Srujan <srujanmhase5@gmail.com>
2023-01-06 14:57:02 +0530

    ref to providers in multiprovider

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,27 @@
 import 'package:cashcalci/app/app.dart';
 import 'package:cashcalci/bootstrap.dart';
+import 'package:cashcalci/providers/client.dart';
+import 'package:cashcalci/providers/rate_provider.dart';
+import 'package:cashcalci/providers/symbols_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  bootstrap(() => const App());
+  bootstrap(
+    () => MultiProvider(
+      providers: [
+        Provider(create: (context) => Client.create()),
+        ProxyProvider<Client, RateProvider>(
+          update: (context, value, prev) => RateProvider(
+            value.client,
+          ),
+        ),
+        ProxyProvider<Client, SymbolsProvider>(
+          update: (context, value, previous) => SymbolsProvider(
+            value.client,
+          ),
+        ),
+      ],
+      child: const App(),
+    ),
+  );
 }
