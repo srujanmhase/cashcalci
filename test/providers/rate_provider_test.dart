7a93ea4b9c59a2883ce7c7f50a55183634a1ee4a
Srujan <srujanmhase5@gmail.com>
2023-01-08 02:54:57 +0530

    rate provider test, symbols test

--- test/providers/rate_provider_test.dart
+++ test/providers/rate_provider_test.dart
@@ -1,7 +1,77 @@
+import 'package:cashcalci/models/latest/latest.dart';
+import 'package:cashcalci/providers/rate_provider.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockDio extends Mock implements Dio {}
 
 void main() {
-  test('rate provider ...', () async {
-    // TODO: Implement test
+  late MockDio client;
+  late RateProvider provider;
+
+  setUp(() {
+    client = MockDio();
+    provider = RateProvider(client);
+  });
+
+  final map = <String, dynamic>{
+    'rates': {
+      'USD': 1,
+    },
+    'timestamp': 0
+  };
+
+  group('Rate provider ... ', () {
+    test('return rates when status code is 200', () async {
+      when(() => client.get<dynamic>(any())).thenAnswer((invocation) async {
+        return Response(
+          statusCode: 200,
+          requestOptions: RequestOptions(path: 'path'),
+          data: map,
+        );
+      });
+
+      final data = Latest.fromJson(map);
+
+      final response = await provider.fetchRates(
+        base: 'base',
+        symbol: 'symbol',
+      );
+
+      expect(response, data);
+    });
+
+    test('throw NoConnection when DioErrorType.other', () async {
+      when(() => client.get<dynamic>(any())).thenThrow(
+        DioError(
+          requestOptions: RequestOptions(path: 'path'),
+          type: DioErrorType.other,
+        ),
+      );
+
+      final response = provider.fetchRates(
+        base: 'base',
+        symbol: 'symbol',
+      );
+
+      await expectLater(response, throwsA(isA<NoConnection>()));
+    });
+
+    test('throw generic Exception when =! DioErrorType.other', () async {
+      when(() => client.get<dynamic>(any())).thenThrow(
+        DioError(
+          requestOptions: RequestOptions(path: 'path'),
+          type: DioErrorType.connectTimeout,
+        ),
+      );
+
+      final response = provider.fetchRates(
+        base: 'base',
+        symbol: 'symbol',
+      );
+
+      await expectLater(response, throwsA(isA<Exception>()));
+    });
   });
 }
--- test/providers/symbols_provider_test.dart
+++ test/providers/symbols_provider_test.dart
@@ -1,7 +1,58 @@
+import 'dart:io';
+
+import 'package:cashcalci/models/symbols/symbols.dart';
+import 'package:cashcalci/providers/symbols_provider.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockDio extends Mock implements Dio {}
 
 void main() {
-  test('symbols provider ...', () async {
-    // TODO: Implement test
+  late MockDio client;
+  late SymbolsProvider provider;
+
+  setUp(() {
+    client = MockDio();
+    provider = SymbolsProvider(client);
+  });
+
+  final map = {
+    'symbols': {'INR': 'Indian Rupees'}
+  };
+
+  group('symbols provider test ... ', () {
+    test('return symbols when status code is 200', () async {
+      when(() => client.get<dynamic>(any())).thenAnswer((invocation) async {
+        return Response(
+          statusCode: 200,
+          requestOptions: RequestOptions(path: 'path'),
+          data: map,
+        );
+      });
+      final response = await provider.fetchSymbols();
+      final data = Symbols.fromJson(map);
+      expect(response, data);
+    });
+
+    test('throw socketexception', () async {
+      when(() => client.get<dynamic>(any())).thenThrow(
+        const SocketException('message'),
+      );
+
+      final future = provider.fetchSymbols();
+
+      await expectLater(future, throwsA(isA<SocketException>()));
+    });
+
+    test('throw generic exception', () async {
+      when(() => client.get<dynamic>(any())).thenThrow(
+        Exception('message'),
+      );
+
+      final future = provider.fetchSymbols();
+
+      await expectLater(future, throwsA(isA<Exception>()));
+    });
   });
 }
